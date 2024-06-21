21b8b1d46e0ea7f43b6e0c0657e15444ddb4d5d1
Teddy Pascual <teddylayson2000@gmail.com>
2024-06-21 11:04:31 +0800

    add user manager page

---
+++ app/admin/user-manager/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userManager = () => {
+  return (
+    <div className="w-screen min-h-screen p-2">
+      <h1 className="text-4xl text-center">User Manager</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default userManager;
